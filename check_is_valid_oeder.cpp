a25360ef17505e6f3f1721516186ee4be07da082
Sheen <44117217+GXUer@users.noreply.github.com>
2020-08-09 22:05:07 +0800

    Update check_is_valid_oeder.cpp

--- check_is_valid_oeder.cpp
+++ check_is_valid_oeder.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+/*----检查序列的合法性：poj=1363题-----
+解析：复杂度O(n)
+*/
 
 bool check_is_valid_oeder(std::queue<int> &order){
-  std::stack<int> s;
-  int n = order.size();
+  std::stack<int> s; //模拟数据出入过程的栈
+  int n = order.size(); 
   for(int i; i < n; i++){
     s.push(i);
-    while(order.front() != s.top()){
-      s.pop();
-	  order.pop();
+    while(!s.empty() && order.front() == s.top()){ //
+		s.pop();
+		order.pop();
     }
-	if(s.empty()){
+	if(!s.empty()){
 		return false;
 	}
   }
