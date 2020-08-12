e40a69c1203171e4c4c6dd0ae1783f4d8b184bce
Sheen <44117217+GXUer@users.noreply.github.com>
2020-08-12 17:06:32 +0800

    Update BSTinsert.cpp

--- BSTinsert.cpp
+++ BSTinsert.cpp
@@ -9,9 +9,9 @@ typedef struct T{
 }TreeNode;
 
 void BST_insert(TreeNode *node, TreeNode *insert_node){
-	if(insert_node->val < node->val){ //
-		if(node->left){ //
-			BST_insert(node->left, insert_node);
+	if(insert_node->val < node->val){ //判断插入结点的值和该节点的大小关系
+		if(node->left){ //左子树不为空插入左子树
+			BST_insert(node->left, insert_node); //递归
 		}
 		else{
 			node->left = insert_node;
