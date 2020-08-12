926ad3e47e800be8cbccff911d076e5d59ba1db0
Sheen <44117217+GXUer@users.noreply.github.com>
2020-08-12 17:04:38 +0800

    Update BSTinsert.cpp

--- BSTinsert.cpp
+++ BSTinsert.cpp
@@ -7,3 +7,29 @@ typedef struct T{
 	TreeNode(int x):
 		val(x), left(NULL), right(NULL) {}
 }TreeNode;
+
+void BST_insert(TreeNode *node, TreeNode *insert_node){
+	if(insert_node->val < node->val){ //
+		if(node->left){ //
+			BST_insert(node->left, insert_node);
+		}
+		else{
+			node->left = insert_node;
+		}
+	}
+	else{
+		if(node->right){
+			BST_insert(node->right, insert_node)	
+		}
+		else{
+			node->right = insert_node; //
+		}
+	}	
+}
+
+
+int main(){
+	
+	
+	return 0;
+}
